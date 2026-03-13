6f2706bbb6477cbdeab511562931420564d9a534
Vladimir V. M. <vvmspace@gmail.com>
2026-03-13 05:56:21 +0400

    feat<pm2>: pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  apps: [
+    {
+      name: "ry-api",
+      script: "./src/api/server.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "ry-jobs-list",
+      script: "./src/workers/jobsListWorker.js",
+      instances: 1,
+      autorestart: false,
+      cron_restart: "0 * * * *",
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "ry-jobs-parse",
+      script: "./src/workers/jobPageWorker.js",
+      instances: 1,
+      autorestart: true,
+      restart_delay: 60000,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "ry-cv-generate",
+      script: "./src/workers/cvGenerationWorker.js",
+      instances: 1,
+      autorestart: true,
+      restart_delay: 120000,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
